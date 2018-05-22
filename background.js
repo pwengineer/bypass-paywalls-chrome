cfd813c524a4e650e72891281fbc32ab1ac90207
Jon Stevens <latchkey@gmail.com>
2018-05-22 08:17:38 +0700

    remove GA tracking
    do we really need to track people using this? come on.

--- background.js
+++ background.js
@@ -227,13 +227,3 @@ chrome.webRequest.onCompleted.addListener(function(details) {
 }, {
   urls: ["<all_urls>"]
 });
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-69824169-2']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
